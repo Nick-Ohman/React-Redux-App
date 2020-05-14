f822439c5d0e937eacea20d3fea4f9b0428d9535
Nick Ohman <nickohman@Nicks-MacBook-Pro.local>
2020-05-14 10:16:49 -0500

    done

--- app/src/components/card.js
+++ app/src/components/card.js
@@ -5,9 +5,10 @@ import React from 'react'
 const Card = props => {
     return (
         <div className="card">
+                    
                     <img src={props.item.image} />
                     <h1>{props.item.name}</h1>
-                    <h2>{props.item.symbol}</h2>
+                    
                     <p>Current Price: ${props.item.current_price}</p>
                     <p>Market Cap: {props.item.market_cap_rank}</p>
             
